6ce475c0a0b37d5daf6ac76ea164d765c0f8f61d
Isma3l-19 <ismaelnyambu.99@gmail.com>
2026-03-03 17:50:25 +0300

    Refactor database connection setup and add product model definition

--- database.py
+++ database.py
@@ -1,13 +1,23 @@
-import os
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 load_dotenv()
 
-connection_string = os.getenv("connection_string")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+connection_string = (
+    f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 engine = create_engine(connection_string)
+
 # creating a session
 session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
-
